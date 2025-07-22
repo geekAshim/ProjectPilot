02c394d6ef354d541bbab81c35d626046df18a76
Ashim Pal <ashimzid@gmail.com>
2025-07-22 12:05:07 +0930

    Lab 10: Creating form to edit data

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,4 +1,6 @@
 import { Project } from './Project';
+import ProjectCard from './ProjectCard';
+import ProjectForm from './ProjectForm';
 
 interface ProjectListProps {
   projects: Project[];
@@ -9,20 +11,12 @@ function ProjectList({ projects }: ProjectListProps) {
     <div className="row">
       {projects.map((project) => (
         <div key={project.id} className="cols-sm">
-          <div className="card">
-            <img src={project.imageUrl} alt={project.name} />
-            <section className="section dark">
-              <h5 className="strong">
-                <strong>{project.name}</strong>
-              </h5>
-              <p>{project.description}</p>
-              <p>Budget : {project.budget.toLocaleString()}</p>
-            </section>
-          </div>
+           <ProjectCard project={project} />
+            <ProjectForm />
         </div>
       ))}
     </div>
-  );
+  );   
 }
 
 export default ProjectList;
