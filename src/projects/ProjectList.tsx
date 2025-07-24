736d07e98a3375ec9ac5652318a34998ee1313d7
Ashim Pal <ashimzid@gmail.com>
2025-07-24 15:37:13 +0930

    LAB 12: onEdit handler implementation

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Project } from './Project';
 import ProjectCard from './ProjectCard';
 import ProjectForm from './ProjectForm';
@@ -7,12 +8,20 @@ interface ProjectListProps {
 }
 
 function ProjectList({ projects }: ProjectListProps) {
+  const [projectBeingEdited, setProjectBeingEdited] = useState({});
+  const handleEdit = (project: Project) => {
+     //console.log(project);
+     setProjectBeingEdited(project);
+   };
   return (
     <div className="row">
       {projects.map((project) => (
-        <div key={project.id} className="cols-sm">
-           <ProjectCard project={project} />
+        <div key={project.id} className="cols-sm">           
+            {project === projectBeingEdited ? (
             <ProjectForm />
+            ) : (
+            <ProjectCard project={project} onEdit={handleEdit} />
+          )}
         </div>
       ))}
     </div>
