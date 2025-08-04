5085c631408955122391d3d1c2e79d6581ba9e54
Ashim Pal <ashimzid@gmail.com>
2025-08-04 12:58:28 +0930

    LAB 18: HTTP GET

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,10 +1,66 @@
-import { MOCK_PROJECTS } from "./MockProjects";
+//import { MOCK_PROJECTS } from "./MockProjects";
 import ProjectList from './ProjectList';
 import { Project } from "./Project";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { projectAPI } from "./projectAPI";
 
 function ProjectsPage() {
-  const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+  //const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | undefined>(undefined);
+  const [currentPage, setCurrentPage] = useState(1);
+
+const handleMoreClick = () => {
+    setCurrentPage((currentPage) => currentPage + 1);
+  };
+
+// Approach 1: using promise then
+//  useEffect(() => {
+//    setLoading(true);
+//    projectAPI
+//      .get(1)
+//      .then((data) => {
+//        setError(null);
+//        setLoading(false);
+//        setProjects(data);
+//      })
+//      .catch((e) => {
+//        setLoading(false);
+//        setError(e.message);
+//        if (e instanceof Error) {
+//           setError(e.message);
+//        }
+//      });
+//  }, []);
+
+ // Approach 2: using async/await
+  useEffect(() => {
+    async function loadProjects() {
+      setLoading(true);
+      try {
+        //const data = await projectAPI.get(1);
+        const data = await projectAPI.get(currentPage);
+        setError('');
+        //setProjects(data);
+        if (currentPage === 1) {
+          setProjects(data);
+        } else {
+          setProjects((projects) => [...projects, ...data]);
+        }
+      }
+       catch (e) {
+        if (e instanceof Error) {
+          setError(e.message);
+        }
+        } finally {
+        setLoading(false);
+      }
+    }
+    loadProjects();
+  }, [currentPage]);
+
+
   const saveProject = (project: Project) => {
     //console.log('Saving project ', project);
     let updatedProjects = projects.map((p: Project) => {
@@ -15,7 +71,40 @@ function ProjectsPage() {
   return (
     <>
         <h1>Projects</h1>
-        <ProjectList projects={MOCK_PROJECTS} onSave={saveProject} />
+
+        {error && (
+        <div className="row">
+          <div className="card large error">
+            <section>
+              <p>
+                <span className="icon-alert inverse "></span>
+                {error}
+              </p>
+            </section>
+          </div>
+        </div>
+      )}
+
+        <ProjectList projects={projects} onSave={saveProject} />
+
+        {!loading && !error && (
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="button-group fluid">
+              <button className="button default" onClick={handleMoreClick}>
+                More...
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+         {loading && (
+        <div className="center-page">
+          <span className="spinner primary"></span>
+          <p>Loading...</p>
+        </div>
+      )}
     </>
   );
 }
